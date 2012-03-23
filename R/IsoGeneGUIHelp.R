e1f79e4ac06855a0fb0afd06015f1b6eb3a035ef
unknown <unknown>
2012-03-23 23:08:48 +0000

    Modify html help files
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IsoGeneGUI@64385 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/IsoGeneGUIHelp.R
+++ R/IsoGeneGUIHelp.R
@@ -1,7 +1,7 @@
 `IsoGeneGUIHelp` <-
 function() {
           tkgrab.release(window)
-    	    helpIndex <- file.path(system.file("doc",package="IsoGeneGUI"),"index.html")
+    	    helpIndex <- file.path(system.file("inst",package="IsoGeneGUI"),"index.html")
           browseURL(helpIndex)
             }
 
